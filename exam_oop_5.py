f910e172849234fdba066efea95300bb06f7e429
amirkle27 <amirkle@gmail.com>
2025-03-26 13:25:21 +0200

    Add files via upload

---
+++ exam_oop_5.py
@@ -0,0 +1,59 @@
+array1 = [1,1,10,10,9,9,5,5,6,8,8]
+array2 = [1,3,3]
+array3 = [9,9,4]
+
+index1 = 1
+index0 = 0
+[print(i) for i in array1 if array1.count(i) == 1]
+[print(i) for i in array2 if array2.count(i) == 1]
+[print(i) for i in array3 if array3.count(i) == 1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #     if not letter in word:
+        #         woo = None
+        #
+        #     print(letter, word)
+        # count += 1
+        # print(word)
+        # for letter in word:
+        #     if all(letter) in word:
+        #         print(word)
+        #     count +=1
+        # print(letter,count)
+
+        #
+        #     count +=1
+
+        #
+        # print(word, count)
+
+
+    #
+    #         count +=1
+    # print(word, count)
+    # count = 0
+# print(w)
+
+
+
+# ( "java", "aavj", "vjaa" ) – 3 פעמים
+# "jijava" – 1 פעמים
+# "vaj" – 1 פעמים
+# "j" – 1 פעמים
+# ( "dan", "and" ) – 2 פעמים
+# "ddan" – 1 פעמים
+#
+# מותר לעבור על הרשימה רק פעם אחת
+
